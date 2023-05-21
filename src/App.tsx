c51345bf7f099805f32cab9e8aec011b79d488c2
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-05-21 14:46:35 +0000

    added disconnect button

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { PublicKey, Transaction } from "@solana/web3.js";
 
-// import functionalities
-import React from 'react';
-import './App.css';
-import {
-  PublicKey,
-  Transaction,
-} from "@solana/web3.js";
-import {useEffect , useState } from "react";
-import './App.css'
-
-// create types
 type DisplayEncoding = "utf8" | "hex";
 
 type PhantomEvent = "disconnect" | "connect" | "accountChanged";
@@ -24,7 +16,6 @@ interface ConnectOpts {
   onlyIfTrusted: boolean;
 }
 
-// create a provider interface (hint: think of this as an object) to store the Phantom Provider
 interface PhantomProvider {
   publicKey: PublicKey | null;
   isConnected: boolean | null;
@@ -40,10 +31,7 @@ interface PhantomProvider {
   request: (method: PhantomRequestMethod, params: any) => Promise<unknown>;
 }
 
- /**
- * @description gets Phantom provider, if it exists
- */
- const getProvider = (): PhantomProvider | undefined => {
+const getProvider = (): PhantomProvider | undefined => {
   if ("solana" in window) {
     // @ts-ignore
     const provider = window.solana as any;
@@ -52,74 +40,88 @@ interface PhantomProvider {
 };
 
 export default function App() {
-  // create state variable for the provider
   const [provider, setProvider] = useState<PhantomProvider | undefined>(
     undefined
   );
 
-	// create state variable for the wallet key
   const [walletKey, setWalletKey] = useState<PhantomProvider | undefined>(
-  undefined
+    undefined
   );
 
-  // this is the function that runs whenever the component updates (e.g. render, refresh)
   useEffect(() => {
-	  const provider = getProvider();
+    const provider = getProvider();
 
-		// if the phantom provider exists, set this as the provider
-	  if (provider) setProvider(provider);
-	  else setProvider(undefined);
+    if (provider) setProvider(provider);
+    else setProvider(undefined);
   }, []);
 
-  /**
-   * @description prompts user to connect wallet if it exists.
-	 * This function is called when the connect wallet button is clicked
-   */
   const connectWallet = async () => {
     // @ts-ignore
     const { solana } = window;
 
-		// checks if phantom wallet exists
     if (solana) {
       try {
-				// connects wallet and returns response which includes the wallet public key
         const response = await solana.connect();
         console.log('wallet account ', response.publicKey.toString());
-				// update walletKey to be the public key
         setWalletKey(response.publicKey.toString());
       } catch (err) {
-      // { code: 4001, message: 'User rejected the request.' }
+        // { code: 4001, message: 'User rejected the request.' }
+        console.log(err);
       }
     }
   };
 
-	// HTML code for the app
+  const disconnectWallet = async () => {
+    if (provider) {
+      try {
+        await provider.disconnect();
+        setWalletKey(undefined);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h2>Connect to Phantom Wallet</h2>
-      {provider && !walletKey && (
-      <button
-        style={{
-          fontSize: "16px",
-          padding: "15px",
-          fontWeight: "bold",
-          borderRadius: "5px",
-        }}
-        onClick={connectWallet}
-      >
-        Connect Wallet
-      </button>
+        {provider && !walletKey && (
+          <button
+            style={{
+              fontSize: "16px",
+              padding: "15px",
+              fontWeight: "bold",
+              borderRadius: "5px",
+            }}
+            onClick={connectWallet}
+          >
+            Connect Wallet
+          </button>
         )}
-        {provider && walletKey && <p>Connected account</p> }
-
+        {provider && walletKey && (
+          <>
+            <p>Connected account</p>
+            <button
+              style={{
+                fontSize: "16px",
+                padding: "15px",
+                fontWeight: "bold",
+                borderRadius: "5px",
+              }}
+              onClick={disconnectWallet}
+            >
+              Disconnect Wallet
+            </button>
+          </>
+        )}
         {!provider && (
           <p>
             No provider found. Install{" "}
             <a href="https://phantom.app/">Phantom Browser extension</a>
           </p>
         )}
-        </header>
+      </header>
     </div>
   );
 }
